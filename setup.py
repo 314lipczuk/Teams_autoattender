6bf2fe42be74abee8f8588da03ec5e4469534fb0
314lipczuk <3.14lipczuk@gmail.com>
2020-12-13 22:58:04 +0100

    basic functionality of setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ import json
 import sys
 import time
 from studiobot import login
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 
 ###########################
 # This script is supposed to set up your credentials file and more importantly, your table file.
-# Second one is important, because that's where bot takes info about timestamps from
+# Second one is important, because that's where bot takes info about timestamps from.
+# Format of table file is [[date-tid of the team],[day,hour_of_start,minute_of_start],[day,hour_of_end,minute_of_end], ...] converted to json
 ###########################
 table=[]
 weekdays = ["Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
@@ -17,8 +20,11 @@ crds += input("Please input password: ")
 f.write(crds)
 f.close()
 driver = login(None)
-cards = driver.find_elements_by_class_name("team-card")
-for card in cards:
+all_cards = driver.find_elements_by_class_name("team-card")
+for x in range(len(all_cards)-1):
+    print("Chceking card nr: ", x+1)
+    cards = driver.find_elements_by_class_name("team-card")
+    card = cards[x]
     arr = [[],[],[]]
     name = card.get_attribute("data-tid")
     arr[0].append(name)
@@ -26,9 +32,20 @@ for card in cards:
     card.click()
     time.sleep(10)
     print("Looking for label")
-    label = driver.find_element_by_class_name("time")
+    try:
+        label = driver.find_element_by_class_name("time")
+    except NoSuchElementException:
+        print("No label found for "+name)
+        driver.back()
+        time.sleep(10)
+        continue 
     print("label found: "+ label.text+"\nParsing commences...")
-    label = label.text.split(" ")[2:]
+    label = label.text.split(" ")
+    if(label[1]!="every"):
+        print("format not yet supported")
+        driver.back()
+        continue
+    label=label[2:]
     #setting day 
     for x in range(7):
         if(weekdays[x]==label[0]):
@@ -37,23 +54,24 @@ for card in cards:
             arr[2].append(x)
     #setting stating time
     stime = label[1][1:].split(":")
-    print(stime)
-    arr[1].append(stime[0])
-    arr[1].append(stime[1])
+    arr[1].append(int(stime[0]))
+    arr[1].append(int(stime[1]))
     #setting ending time
     stime[0] = int(stime[0])+1
     stime[1] = int(stime[1])+30
     if int(stime[1]) >=60:
         stime[0]=int(stime[0])+1
         stime[1]=int(stime[1])-60
-
     arr[2].append(stime[0])
     arr[2].append(stime[1])
+    table.append(arr)
+    print(table)
+    driver.back()
+    time.sleep(10)
 
 
-    print(arr)
-    time.sleep(60*10)
-    
-    
-
-
+tablejson=json.dumps(table)
+f = open("tablefile.json","w")
+f.write(tablejson)
+f.close()
+print("All done, check if it's all there")
